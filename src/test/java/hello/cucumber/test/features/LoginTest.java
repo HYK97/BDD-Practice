e25217e81d1d965915f38fbc58415bf1bc8feab9
ddh963963 <ddh963963@naver.com>
2022-07-29 12:49:05 +0900

    Feat : 수정사항

--- src/test/java/hello/cucumber/test/features/LoginTest.java
+++ src/test/java/hello/cucumber/test/features/LoginTest.java
@@ -1,45 +1,19 @@
 package hello.cucumber.test.features;
 
-import hello.cucumber.test.entity.User;
-import hello.cucumber.test.repository.UserRepository;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Scope;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static hello.cucumber.test.entity.User.*;
-import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
-import static org.assertj.core.api.Assertions.assertThat;
-
-@Scope(SCOPE_CUCUMBER_GLUE)
 public class LoginTest extends SpringInit {
-    List<User> users =new ArrayList<>();
-    @Autowired
-    private UserRepository userRepository;
 
     @Given("다음 계정들이 회원가입된다 {string} {string}")
     public void given1(String id, String password) {
-        User user = User.builder().userid(id).password(password).build();
-        User saveUser = userRepository.save(user);
-        users.add(saveUser);
+        register(id, password);
     }
+
     @Then("회원가입이 잘됬는지 확인 {string} {string}")
     public void then1(String id, String password) {
-        for (User user : users) {
-            System.out.println("user = " + user);
-        }
-        User findUser = userRepository.findByUserid(id);
-
+        registerTest(id, password);
     }
 
 }
--- src/test/java/hello/cucumber/test/features/SpringInit.java
+++ src/test/java/hello/cucumber/test/features/SpringInit.java
@@ -1,15 +1,33 @@
 package hello.cucumber.test.features;
 
+import hello.cucumber.test.TestApplication;
+import hello.cucumber.test.entity.User;
 import hello.cucumber.test.repository.UserRepository;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.assertj.core.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Scope;
 
-import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @CucumberContextConfiguration
-@SpringBootTest
+@SpringBootTest(classes = TestApplication.class)
 public class SpringInit {
+
+    List<User> users = new ArrayList<>();
+    @Autowired
+    private UserRepository userRepository;
+
+    public void register(String id, String password) {
+        User user = User.builder().userid(id).password(password).build();
+        User saveUser = userRepository.save(user);
+        users.add(saveUser);
+    }
+
+    public void registerTest(String id, String password) {
+        User findUser = userRepository.findByUserid(id);
+        Assertions.assertThat(users.get(0)).isEqualTo(findUser);
+    }
 }
