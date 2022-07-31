5be3bef85cb914a81f1caf6214ef589ede3fff72
ddh963963 <ddh963963@naver.com>
2022-07-31 21:42:01 +0900

    Feat : DataTable 제거 Test 간소화

--- src/test/java/hello/cucumber/test/features/LoginTest.java
+++ src/test/java/hello/cucumber/test/features/LoginTest.java
@@ -1,14 +1,11 @@
 package hello.cucumber.test.features;
 
 import hello.cucumber.test.entity.User;
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
@@ -27,64 +24,43 @@ public class LoginTest extends SpringInit {
     }
 
 
-    @Given("다음 계정들이 회원가입된다 :")
-    public void given1(DataTable data) {
-        List<String> row = data.row(0);
-        String id = row.get(0);
-        System.out.println("id = " + id);
-        String password = row.get(1);
-        System.out.println("password = " + password);
+    @Given("다음 계정들이 회원가입된다 {string} {string}")
+    public void given1(String id, String password) {
         register(id, password);
     }
 
-    @Then("회원가입 확인 :")
-    public void then1(DataTable data) {
-        List<String> row = data.row(0);
-        String id = row.get(0);
-        String password = row.get(1);
+    @Then("회원가입 확인 {string} {string}")
+    public void then1(String id, String password) {
         registerTest(id, password);
     }
 
 
-    @When("로그인시 아이디는 비워둘수없습니다. 메세지 띄움 :")
-    public void exception1(DataTable data) {
-        List<String> row = data.row(0);
-        String id = row.get(0);
-        String password = row.get(1);
-        idNotBlankTest(id, password);
+    @When("로그인시 아이디는 비워둘수없습니다. 메세지 띄움 {string}")
+    public void exception1(String password) {
+        idNotBlankTest(null, password);
     }
 
-    @When("로그인시 비밀번호는 비워둘수없습니다. 메세지 띄움 :")
-    public void exception2(DataTable data) {
-        List<String> row = data.row(0);
-        String id = row.get(0);
-        String password = row.get(1);
-        passwordNotBlankTest(id, password);
+    @When("로그인시 비밀번호는 비워둘수없습니다. 메세지 띄움 {string}")
+    public void exception2(String id) {
+        passwordNotBlankTest(id, null);
     }
 
-    @When("비밀번호가 다를경우 IllegalArgumentException 발생 :")
-    public void exception3(DataTable data) {
-        List<String> row = data.row(0);
-        String id = row.get(0);
-        String password = row.get(1);
+    @When("비밀번호가 다를경우 로그인 실패 {string} {string}")
+    public void exception3(String id, String password) {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> loginTest(id, password));
     }
 
-    @When("아이디가 다를경우 IllegalArgumentException 발생 :")
-    public void exception4(DataTable data) {
-        List<String> row = data.row(0);
-        String id = row.get(0);
-        String password = row.get(1);
+    @When("아이디가 다를경우 로그인 실패 {string} {string}")
+    public void exception4(String id, String password) {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> loginTest(id, password));
     }
 
-    @When("로그인 성공 했을 경우 :")
-    public void success(DataTable data) {
-        List<String> row = data.row(0);
-        String id = row.get(0);
-        String password = row.get(1);
+    @When("로그인 성공 했을 경우 {string} {string}")
+    public void success(String id, String password) {
         User user = loginTest(id, password);
         assertThat(user.getUserid()).isEqualTo(id);
         assertThat(user.getPassword()).isEqualTo(password);
     }
+
+
 }
--- src/test/resources/hello/cucumber/test/features/user_login.feature
+++ src/test/resources/hello/cucumber/test/features/user_login.feature
@@ -1,27 +1,20 @@
 Feature: 유저 로그인
 
   Background:
-    Given 다음 계정들이 회원가입된다 :
-      | user1 | password1 |
-    Then 회원가입 확인 :
-      | user1 | password1 |
+    Given 다음 계정들이 회원가입된다 "user1" "password1"
+    Then 회원가입 확인 "user1" "password1"
 
   Scenario: 유저 로그인 실패 시나리오 id가 빈 공간일 경우
-    When 로그인시 아이디는 비워둘수없습니다. 메세지 띄움 :
-      |  | password1 |
+    When 로그인시 아이디는 비워둘수없습니다. 메세지 띄움 "password1"
 
   Scenario: 유저 로그인 실패 시나리오  password가 빈 공간일 경우
-    When 로그인시 비밀번호는 비워둘수없습니다. 메세지 띄움 :
-      | user1 |  |
+    When 로그인시 비밀번호는 비워둘수없습니다. 메세지 띄움 "user1"
 
   Scenario: 유저 로그인 실패 시나리오 비밀번호가 다를 경우
-    When 비밀번호가 다를경우 IllegalArgumentException 발생 :
-      | user1 | password2d |
+    When 비밀번호가 다를경우 로그인 실패 "user1" "password2d"
 
   Scenario: 유저 로그인 실패 시나리오 아이디가 다를경우
-    When  아이디가 다를경우 IllegalArgumentException 발생 :
-      | user2 | password1 |
+    When  아이디가 다를경우 로그인 실패 "user2" "password1"
 
   Scenario: 유저 로그인 성공 시나리오
-    When 로그인 성공 했을 경우 :
-      | user1 | password1 |
+    When 로그인 성공 했을 경우 "user1" "password1"
